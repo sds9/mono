946ff0de6bae1f059571b0afa26dbdcff2e9e241
Panda Noodles <pandanoodles416@users.noreply.github.com>
2025-06-29 19:30:10 -0500

    feat: Update ESLint configuration and scripts for CDK and TypeScript support

--- cdk/.eslintrc.js
+++ cdk/.eslintrc.js
@@ -1,10 +1,8 @@
 module.exports = {
   root: true,
   env: {
-    browser: true,
-    es2020: true,
     node: true,
-    jest: true,
+    es2020: true,
   },
   extends: [
     'eslint:recommended',
@@ -15,13 +13,8 @@ module.exports = {
   },
   rules: {
     'no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
+    'no-console': 'off',
   },
-  // Only lint JS files at the root level, ignore generated files
-  ignorePatterns: [
-    'node_modules/**',
-    '**/lib/**',
-    '**/dist/**',
-    '**/*.d.ts',
-    'cdk/**', // CDK files are linted separately
-  ],
+  // Ignore TypeScript-specific files for now - they'll be checked by tsc
+  ignorePatterns: ['**/*.d.ts', 'lib/**/*'],
 };
